71a4ac90612e2cdbe0bb12775ad5b4f8d88314b0
Damola Adediran <tylher123@gmail.com>
2022-08-13 19:42:50 +0100

    Add new migration

--- db/migrate/20220813184215_add_category_ref_to_category_deals.rb
+++ db/migrate/20220813184215_add_category_ref_to_category_deals.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddCategoryRefToCategoryDeals < ActiveRecord::Migration[7.0]
   def change
     add_reference :category_deals, :category, null: false, foreign_key: true
