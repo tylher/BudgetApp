7d74eddd9eb1642da4faf8044789299d9fd03bb1
Damola Adediran <tylher123@gmail.com>
2022-08-19 12:26:49 +0100

    change after signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :categories do
     resources :deals, only: %i[new create destroy]
   end
